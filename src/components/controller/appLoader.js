2b858c58cab12a7e0f14a1d95fc5c196b428d3e1
Davydau Aliaksey <davidoff.aleksei@gmail.com>
2021-11-25 02:29:10 +0300

    init: init project. fix build output folder

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '66048ed48bba4266a3ef2f842906e98d', // получите свой ключ https://newsapi.org/
         });
     }
 }
