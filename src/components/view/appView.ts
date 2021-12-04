22a2e66d9f0b29a8945392f9b05feff6acc985c3
davidoff-a <davidoff.aleksei@gmail.com>
2021-12-04 10:47:19 +0300

    feat: migrate appView.ts to typesctript

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,20 @@
-import News from './news/news';
-import Sources from './sources/sources';
+const News = require('./news/news');
+const Sources = require('./sources/sources');
 
 export class AppView {
+  news: any;
+  sources: any;
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
+  drawNews(data: any): void {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  drawSources(data: any): void {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
